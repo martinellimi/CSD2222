2a9b0105b9af4871ba2235f8e43978e1e4dbc06f
martinellimi <martinellimi@milenas-mbp-2.uni.mdx.ac.uk>
2014-11-05 16:38:30 +0000

    Student Class

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+
+public class Student {
+
+	public Student(String name, Integer age, char gender, String module) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.module = module;
+	}
+	
+	private String name;
+	private Integer age;
+	private char gender;
+	private String module;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+}
